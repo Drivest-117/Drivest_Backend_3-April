c4f54f2aeeded9f003a434dec16f283cdcf70dbd
SooshIT <adminM@soosh.co.uk>
2026-02-22 22:43:17 +0000

    chore(api): log 500 exceptions in global filter

--- src/common/http-exception.filter.ts
+++ src/common/http-exception.filter.ts
@@ -4,10 +4,13 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
@@ -21,6 +24,14 @@ export class HttpExceptionFilter implements ExceptionFilter {
       exception instanceof HttpException
         ? exception.getResponse()
         : 'Internal server error';
+
+    if (status >= HttpStatus.INTERNAL_SERVER_ERROR) {
+      const err = exception as Error;
+      this.logger.error(
+        `Unhandled exception (${status})`,
+        err?.stack ?? String(exception),
+      );
+    }
 
     response.status(status).json({
       data: null,
